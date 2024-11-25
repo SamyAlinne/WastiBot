4a01f2364e4841a4591397c45f693719d814d6fe
SamyAlinne <117871370+SamyAlinne@users.noreply.github.com>
2024-11-24 18:30:23 -0600

    También le puse por si no tiene un nombre jijiji

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -14,24 +14,26 @@ interface Movie {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
   private apiUrl = 'http://localhost:3000/api';
   private userName: string = '';
   private currentMovie: Movie | null = null;
   private movieContext: string | null = null;
-  private badWords: string[] = []; 
+  private badWords: string[] = [];
 
   constructor(private http: HttpClient) {
     this.loadBadWords();
-   }
+  }
 
-    // Cargar las malas palabras desde el archivo JSON
-    private loadBadWords() {
-    this.http.get<{ badWords: string[] }>('/assets/bad-words.json').subscribe(data => {
-    this.badWords = data.badWords;  // Almacenamos las malas palabras
-    });
+  // Cargar las malas palabras desde el archivo JSON
+  private loadBadWords() {
+    this.http
+      .get<{ badWords: string[] }>('/assets/bad-words.json')
+      .subscribe((data) => {
+        this.badWords = data.badWords;
+      });
   }
 
   setUserName(name: string) {
@@ -49,22 +51,54 @@ export class ChatService {
   setCurrentMovie(movie: Movie) {
     this.currentMovie = movie;
   }
-  
+
   setMovieContext(context: string) {
     this.movieContext = context;
   }
 
   extractName(message: string): string {
-    const commonWords = ['me', 'llamo', 'soy', 'mi', 'nombre', 'es', 'hola', 'saludos', 'holi', 'holiwis', 'holis', 'buenas', 'wenas', '¡hola!', '¡holis!', 'eres'];
-    const badWords = [,];
-    const cleanMessage = message.replace(/[.,\/#¡!¿?$%\^&\*;:{}=\-_`~()0-9]/g, "");
+    const commonWords = [
+      'me',
+      'llamo',
+      'soy',
+      'mi',
+      'nombre',
+      'es',
+      'hola',
+      'saludos',
+      'holi',
+      'holiwis',
+      'holis',
+      'buenas',
+      'wenas',
+      '¡hola!',
+      '¡holis!',
+      'eres',
+    ];
+    const cleanMessage = message.replace(
+      /[.,\/#¡!¿?$%\^&\*;:{}=\-_`~()0-9]/g,
+      ''
+    );
+    // Verificar si el mensaje está vacío
+    if (!cleanMessage) {
+      return 'Vacio';
+    }
     const words = cleanMessage.toLowerCase().split(/\s+/);
-    const potentialName = words.find(word => !commonWords.includes(word) && word.length > 1);
+    const potentialName = words.find(
+      (word) => !commonWords.includes(word) && word.length > 1
+    );
 
     if (potentialName && this.badWords.includes(potentialName.toLowerCase())) {
-      return 'WastiAmigo';  // Si es una mala palabra, se asigna el nombre WastiAmigo
-  }
-    return potentialName ? potentialName.charAt(0).toUpperCase() + potentialName.slice(1) : '';
+      return 'WastiAmigo'; // Si es una mala palabra, se asigna el nombre WastiAmigo
+    }
+    // Si no se encuentra un nombre válido
+    if (!potentialName) {
+      return 'Vacio';
+    }
+
+    return potentialName
+      ? potentialName.charAt(0).toUpperCase() + potentialName.slice(1)
+      : '';
   }
 
   sendMessage(message: string, isFirstMessage: boolean): Observable<any> {
@@ -73,27 +107,45 @@ export class ChatService {
       this.setUserName(extractedName);
       // Respuesta personalizada si el nombre es una mala palabra
       if (extractedName === 'WastiAmigo') {
-        return of({ message: `Siento mucho que te llames así :c, pero lamentablemente mi Wasti programación no me permite ser malo, así que te llamaré WastiAmigo ;D. <br> <br> ¡Hola <b>WastiAmigo!</b> Encantado de conocerte. ¿Qué película navideña te gustaría descubrir hoy?` });
-    }
-    // Respuesta estándar si el nombre es válido
-    return of({ message: `¡Hola <b>${extractedName}</b>! Encantado de conocerte. ¿Qué película navideña te gustaría descubrir hoy?` });
-}
+        return of({
+          message: `Siento mucho que te llames así :c, pero lamentablemente mi Wasti programación no me permite ser malo, así que te llamaré WastiAmigo ;D. <br> <br> ¡Hola <b>WastiAmigo!</b> Encantado de conocerte. ¿Qué película navideña te gustaría descubrir hoy?`,
+        });
+      }
+      if (extractedName === 'Vacio') {
+        return of({
+          message: `Lamento que no tengas nombre :c, pero por eso estoy aquí, para ayudarte :D. Ahora serás... ¡WastiAmigo! <br> <br> ¡Hola <b>WastiAmigo!</b>  Encantado de conocerte. ¿Qué película navideña te gustaría descubrir hoy?`,
+        });
+      }
 
-    return this.http.post<{ message: string, movie?: Movie, movieContext?: string }>(`${this.apiUrl}/chat`, { message }).pipe(
-      map(response => {
-        if (response.movie) {
-          this.setCurrentMovie(response.movie);
-        }
-        if (response.movieContext) {
-          this.setMovieContext(response.movieContext);
-        }
-        return response;
-      }),
-      catchError(error => {
-        console.error('Error en la comunicación con el servidor:', error);
-        return of({ message: 'Lo siento, ha ocurrido un error. Por favor, intenta de nuevo.' });
-      })
-    );
+      // Respuesta estándar si el nombre es válido
+      return of({
+        message: `¡Hola <b>${extractedName}</b>! Encantado de conocerte. ¿Qué película navideña te gustaría descubrir hoy?`,
+      });
+    }
+
+    return this.http
+      .post<{ message: string; movie?: Movie; movieContext?: string }>(
+        `${this.apiUrl}/chat`,
+        { message }
+      )
+      .pipe(
+        map((response) => {
+          if (response.movie) {
+            this.setCurrentMovie(response.movie);
+          }
+          if (response.movieContext) {
+            this.setMovieContext(response.movieContext);
+          }
+          return response;
+        }),
+        catchError((error) => {
+          console.error('Error en la comunicación con el servidor:', error);
+          return of({
+            message:
+              'Lo siento, ha ocurrido un error. Por favor, intenta de nuevo.',
+          });
+        })
+      );
   }
 
   getTopMovies(): Observable<Movie[]> {
@@ -102,7 +154,11 @@ export class ChatService {
 
   getMovieDetails(title: string): Observable<Movie | null> {
     return this.getTopMovies().pipe(
-      map(movies => movies.find(m => m.titulo.toLowerCase() === title.toLowerCase()) || null)
+      map(
+        (movies) =>
+          movies.find((m) => m.titulo.toLowerCase() === title.toLowerCase()) ||
+          null
+      )
     );
   }
 
@@ -125,24 +181,41 @@ export class ChatService {
   }
 
   detectInfoType(message: string): string {
-    const cleanMessage = message.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()0-9]/g, "").replace(/[¿¡]/g, "");
+    const cleanMessage = message
+      .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()0-9]/g, '')
+      .replace(/[¿¡]/g, '');
     const lowerMessage = cleanMessage.toLowerCase();
     const words = lowerMessage.split(/\s+/);
-  
+
     const infoTypes = {
       año: ['año', 'cuando', 'estreno', 'estrenó', 'lanzó', 'salió', 'fecha'],
       director: ['director', 'dirigió', 'realizó', 'filmó', 'creador'],
-      calificación: ['calificación', 'puntaje', 'rating', 'puntuación', 'nota', 'valoración'],
+      calificación: [
+        'calificación',
+        'puntaje',
+        'rating',
+        'puntuación',
+        'nota',
+        'valoración',
+      ],
       género: ['género', 'tipo', 'categoría', 'estilo'],
-      trama: ['trama', 'argumento', 'historia', 'sinopsis', 'resumen', 'de qué trata', 'de que va']
+      trama: [
+        'trama',
+        'argumento',
+        'historia',
+        'sinopsis',
+        'resumen',
+        'de qué trata',
+        'de que va',
+      ],
     };
-  
+
     for (const [type, keywords] of Object.entries(infoTypes)) {
-      if (keywords.some(keyword => words.includes(keyword))) {
+      if (keywords.some((keyword) => words.includes(keyword))) {
         return type;
       }
     }
-  
+
     return 'general';
   }
 }
